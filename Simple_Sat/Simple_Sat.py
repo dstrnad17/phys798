c463f3d2d0789ce308aba6e609b329b1ed8887e4
leeerikj <99084735+leeerikj@users.noreply.github.com>
2022-07-08 01:42:56 -0400

    Add files via upload

--- Simple_Sat/Simple_Sat.py
+++ Simple_Sat/Simple_Sat.py
@@ -1,25 +1,14 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Sun Mar  6 21:26:04 2022
-
-@author: erikj
-"""
 # Import Numpy & PyTorch
 import torch
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
-import logging, sys, os
+import os
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 from sklearn.preprocessing import MaxAbsScaler
 from sklearn.pipeline import Pipeline
-from sklearn.impute import SimpleImputer
 from sklearn.compose import ColumnTransformer
-
-
-
-
 import torch.nn as nn
 from torch.utils.data import TensorDataset, DataLoader
 
@@ -38,9 +27,12 @@ def xprint(msg):
     f.write(msg + "\n")
     f.close()
 
+# Read in satellite data
+sat_data_df = pd.read_pickle("./sat_data/sat_data_first_file.pkl")
 
-sat_data_df = pd.read_pickle("./sat_data/sat_data.pkl")
-sat_data_df = sat_data_df.sort_values(by='epoch', ascending=True)
+
+plt.savefig('Magnetic Field Measurement Z Component') 
+
 
 position = ["x[km]", "y[km]", "z[km]"]
 field = ["bx[nT]", "by[nT]", "bz[nT]"]
@@ -49,52 +41,49 @@ pipe = Pipeline(steps=[("scaler", StandardScaler()), ("unit", MaxAbsScaler())])
 
 preprocessor_pos = ColumnTransformer(transformers=[("num_pos", pipe, position)])
 preprocessor_field = ColumnTransformer(transformers=[("num_field", pipe, field)])
+Pos_trans = pd.DataFrame(preprocessor_pos.fit_transform(sat_data_df[position]))
+Pos_trans.columns = position
+Field_trans = pd.DataFrame(preprocessor_field.fit_transform(sat_data_df[field]))
+Field_trans.columns = field
+df_pos_field_transformed = pd.concat([Pos_trans, Field_trans], axis = 1, join = 'inner')
+
 
 class SatSimpleNet(nn.Module):
     # Initialize the layers
     def __init__(self):
         super().__init__()
-        self.linear1 = nn.Linear(3, 100)
+        self.linear1 = nn.Linear(3, 50)
         self.act1 = nn.Tanh() # Activation function
-        self.linear2 = nn.Linear(100, 100)
-        self.act2 = nn.Tanh() # Activation function
-        self.linear3 = nn.Linear(100, 3)
+        self.linear2 = nn.Linear(50, 3)
     
     # Perform the computation
     def forward(self, x):
         x = self.linear1(x)
         x = self.act1(x)
         x = self.linear2(x)
-        x = self.act2(x)
-        x = self.linear3(x)
         return x
-    
-train, test = train_test_split(sat_data_df, test_size=0.05)
+
+## Use position data as input
+
+
+train, test = train_test_split(df_pos_field_transformed, test_size=0.20)
 Position_train = train[["x[km]", "y[km]", "z[km]"]]
 Field_train = train[["bx[nT]", "by[nT]", "bz[nT]"]]
 Position_test = test[["x[km]", "y[km]", "z[km]"]]
 Field_test = test[["bx[nT]", "by[nT]", "bz[nT]"]]
 
-preprocessor_pos.fit(Position_train)
-preprocessor_field.fit(Field_train)
-
-
-# Define PyTorch tensors 
-# train_inputs = torch.from_numpy(np.float32(Position_train.iloc[:100000,:].values))
-# train_targets = torch.from_numpy(np.float32(Field_train.iloc[:100000,:].values))
-# test_inputs = torch.from_numpy(np.float32(Position_test.iloc[:100000,:].values))
-# test_targets = torch.from_numpy(np.float32(Field_test.iloc[:100000,:].values))
 
 train_inputs = torch.from_numpy(np.float32(Position_train))
 train_targets = torch.from_numpy(np.float32(Field_train))
 test_inputs = torch.from_numpy(np.float32(Position_test))
 test_targets = torch.from_numpy(np.float32(Field_test))
 
+
 train_ds = TensorDataset(train_inputs, train_targets)
 test_ds = TensorDataset(test_inputs, test_targets)
 
 # Define data loader
-batch_size = 25
+batch_size = 128
 train_dl = DataLoader(train_ds, batch_size, shuffle=False)
 test_dl = DataLoader(test_ds, batch_size, shuffle=False)
 
@@ -107,72 +96,88 @@ loss_fn = F.mse_loss
 
 np.random.seed(123)
 torch.manual_seed(123)
-loss_saved = []
-def train(epoch):
+training_loss_saved = []
+test_loss = []
+def train(num_epochs):
     sat_model.train()
-    for batch_idx, (data, target) in enumerate(train_dl):
-        data, target = Variable(data), Variable(target)
-        opt_n.zero_grad()
-        output = sat_model(data)
-        loss = loss_fn(output, target)
-        loss_saved.append(loss.detach().numpy())
-        xprint('Training iteration loss = '+ str(loss.detach().numpy()) + '\n')
-        loss.backward()
-        opt_n.step()
-    return loss_fn(sat_model(data),target).detach().numpy()
+    for epoch in range (num_epochs):
+        for batch_idx, (data, target) in enumerate(train_dl):
+            result = 0
+            data, target = Variable(data), Variable(target)
+            opt_n.zero_grad()
+            output = sat_model(data)
+            loss = loss_fn(output, target)
+            training_loss_saved.append(loss.detach().numpy())
+            xprint('Training iteration loss = '+ str(loss.detach().numpy()) + '\n')
+            loss.backward()
+            opt_n.step()
+            for test_batch_idx, (data, target) in enumerate(test_dl):
+                test_data, test_target = Variable(data), Variable(target)
+                result = result + loss_fn(sat_model(test_data), test_target)
+            result = result/len(test_dl)
+            test_loss.append(result)
+    return loss.detach().numpy()
 
 def test():
     sat_model.eval()
+    output = []
     for batch_idx, (data, target) in enumerate(test_dl):
         data, target = Variable(data), Variable(target)
-        output = sat_model(data)
-    return loss_fn(output,target).detach().numpy(), output.detach().numpy(), target.detach().numpy()
+        output.append(sat_model(data))
+    return loss_fn(sat_model(data),target).detach().numpy(), output
 
 # Train model 3 for 100 epochs
-sat_nn_regresion_train = train(5)
-#error_train = np.sqrt(np.dot((train_targets.detach().numpy()-sat_nn_regresion_train.detach().numpy()).transpose(),(train_targets.detach().numpy()-sat_nn_regresion_train.detach().numpy())))
-sat_nn_regresion_test, output, target = test()
-#error_test = np.sqrt(np.dot((test_targets.detach().numpy()-sat_nn_regresion_test.detach().numpy()).transpose(),(test_targets.detach().numpy()-sat_nn_regresion_test.detach().numpy())))
+sat_nn_regresion_train = train(10)
+sat_nn_regresion_test, output = test()
+
 
 xprint('Single layer neural network training data error = '+ str(sat_nn_regresion_train) + '\n')
 xprint('Single layer neural network test data error = '+ str(sat_nn_regresion_test))
 
 # Plot data points
-fig, ax = plt.subplots()
-ax.plot(loss_saved)
-plt.xlabel("Batch Number")
-plt.ylabel("Loss")
-plt.title("Training Loss")
-plt.show
-
-plt.savefig('Simple_sat_loss_All_Data.pdf') 
-
+test_loss_saved = ([i.detach().numpy() for i in test_loss])
 fig, bx = plt.subplots()
-bx.plot(target[0])
-bx.plot(output[0])
-bx.legend(['Bx_true', 'output 1','output 2', 'output 3'])
+bx.loglog(training_loss_saved)
+bx.loglog(test_loss_saved)
+plt.legend(['Training Loss', 'Test Loss'], loc = 'upper right')
+plt.text(.5, 0, 'Single layer neural network training data error = '+ str(sat_nn_regresion_train) + '\n' + '\n', horizontalalignment='center',
+     verticalalignment='bottom', transform=bx.transAxes, fontsize = 7)
+plt.text(.5, 0, 'Single layer neural network test data error = '+ str(sat_nn_regresion_test) + '\n', horizontalalignment='center',
+     verticalalignment='bottom', transform=bx.transAxes, fontsize = 7)
+plt.xlabel("Iterations")
+plt.ylabel("Loss")
+plt.title("Training Loss (Position as Input)")
+plt.show
+
+plt.savefig('Simple_sat_loss_Position_Input.pdf') 
+
+output_array = np.concatenate([i.detach().numpy() for i in output])
+title = ['output 1', 'output 2', 'output 3']
+Odf = Field_test
+Odf[title] = output_array
+Odf = Odf.sort_index()
+
+
+Odf.plot(y = ['bx[nT]','output 1'], kind = 'line')
+plt.legend(['Bx_true', 'output 1'])
 plt.ylabel("B_x")
-plt.title("Measured Bx versus Model Output")
+plt.title("Measured Bx versus Model Output (Position as Input)")
 plt.show
 
-plt.savefig('True_vs_Model_Bx_All_Data.pdf') 
+plt.savefig('True_vs_Model_Bx_Position_Input.pdf') 
 
-fig, by = plt.subplots()
-by.plot(target[1])
-by.plot(output[1])
-by.legend(['By_true', 'output 1','output 2', 'output 3'])
+Odf.plot(y = ['by[nT]','output 2'], kind = 'line')
+plt.legend(['By_true', 'output 2'])
 plt.ylabel("B_y")
-plt.title("Measured By versus Model Output")
+plt.title("Measured By versus Model Output (Position as Input)")
 plt.show
 
-plt.savefig('True_vs_Model_By_All_Data.pdf') 
+plt.savefig('True_vs_Model_By_Position_Input.pdf') 
 
-fig, bz = plt.subplots()
-bz.plot(target[2])
-bz.plot(output[2])
-bz.legend(['Bz_true', 'output 1','output 2', 'output 3'])
+Odf.plot(y = ['bz[nT]','output 3'], kind = 'line')
+plt.legend(['Bz_true', 'output 3'])
 plt.ylabel("B_z")
-plt.title("Measured Bz versus Model Output")
+plt.title("Measured Bz versus Model Output (Position as Input)")
 plt.show
 
-plt.savefig('True_vs_Model_Bz_All_Data.pdf') 
+plt.savefig('True_vs_Model_Bz_Position_Input.pdf')
