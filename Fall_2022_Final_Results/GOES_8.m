6094498f89f5a04bc50f05979acef5abf7a3275c
Bob Weigel <Robert.S.Weigel@gmail.com>
2023-02-03 16:41:07 -0500

    Create GOES_8.m

---
+++ Fall_2022_Final_Results/GOES_8.m
@@ -0,0 +1,33 @@
+# Summary
+
+|    | Model NN           |    Bx |    By |    Bz |
+|---:|:-------------------|------:|------:|------:|
+|  0 | All Inputs         | 0.773 | 0.231 | 0.637 |
+|  1 | Remove_r[km]       | 0.751 | 0.099 | 0.236 |
+|  2 | Remove_theta[deg]  | 0.115 | 0.09  | 0.547 |
+|  3 | Remove_phi[deg]    | 0.753 | 0.121 | 0.482 |
+|  4 | Remove_vsw[km/s]   | 0.742 | 0.235 | 0.593 |
+|  5 | Remove_ey[mV/m]    | 0.755 | 0.219 | 0.643 |
+|  6 | Remove_imfbz[nT]   | 0.759 | 0.231 | 0.637 |
+|  7 | Remove_nsw[1/cm^3] | 0.696 | 0.214 | 0.607 |
+
+
+|    | Model Linear       |     Bx |     By |      Bz |
+|---:|:-------------------|-------:|-------:|--------:|
+|  0 | All Inputs Linear  | -1.195 | -9.398 | -11.264 |
+|  1 | Remove r[km]       | -6.789 | -1.025 |  -2.187 |
+|  2 | Remove theta[deg]  | -6.878 | -1.121 |  -2.904 |
+|  3 | Remove phi[deg]    | -7.679 | -1.145 |  -2.825 |
+|  4 | Remove vsw[km/s]   | -8.09  | -2.558 |  -3.751 |
+|  5 | Remove ey[mV/m]    | -8.733 | -2.954 |  -4.036 |
+|  6 | Remove imfbz[nT]   | -8.721 | -2.977 |  -4.091 |
+|  7 | Remove nsw[1/cm^3] | -8.778 | -2.974 |  -4.18  |
+
+# GOES8_1996_Electric_Field_y
+
+![GOES8_1996_Electric_Field_y][GOES8_1996_Electric_Field_y.png]
+
+# GOES8_1996_Electric_Field_y
+
+![GOES8_1996_Electric_Field_y][GOES8_1996_Electric_Field_y.png]
+
